e4aa5ed3f250045c56a52e642b1cb0653580f961
Raphael S. Rodrigues <raph@raph-web.com>
2015-12-07 14:09:43 +0100

    Create Custos.php

---
+++ app/Models/Custos.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Custos extends Model {
+    
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+     
+     protected $table = 'Custos';
+     
+     /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    // Now declare the relationship with "occasion_categories" table
+    public function occasionCategory()
+    {
+        // Make sure you have used occasion_categories_id as the foreugn key
+        return $this->belongsTo('OccasionCategory', 'occasion_categories_id', 'id');
+    }
+}
