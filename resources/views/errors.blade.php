081e59a8dbdfdc6b29836fc6d7ffc7843673cfeb
Raphael S. Rodrigues <raph@raph-web.com>
2015-12-07 13:57:12 +0100

    Update errors.blade.php

--- resources/views/errors.blade.php
+++ resources/views/errors.blade.php
@@ -1,9 +1,34 @@
-@extends('layouts.master')
+@extends('layouts.master2')
 @section('content')
 
- 
+ <div class="text-center">
+        
     
 <?php echo $mensagem; ?>
+
+<?php if (isset($pathname) || isset($urlname)) {
+    
+    echo "<hr>";
+    echo "path name: " .$pathname;
+    echo "<hr>";
+    echo "url name: " .$urlname;
+     echo "<hr>";
+}
+
+?>
+
+// <?php if (isset($value) || isset($data)) {
+//     
+    // echo "<hr>";
+    // echo "path name: " .$value;
+    // echo "<hr>";
+    // echo "url name: " .$data;
+     // echo "<hr>";
+}
+// 
+// ?>
+
+</div>
 
  
 @endsection
